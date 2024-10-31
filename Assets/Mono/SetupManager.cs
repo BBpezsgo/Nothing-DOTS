5dabd037310613b675152e5d0dae71dee3e4313f
BB_pezsgo <bazsi5fel2@gmail.com>
2024-10-31 16:30:39 +0100

    spawn N units (for testing)

--- Assets/Mono/SetupManager.cs
+++ Assets/Mono/SetupManager.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Unity.Entities;
 using Unity.Mathematics;
@@ -15,7 +17,23 @@ public class UnitSetup
 public class SetupManager : Singleton<SetupManager>
 {
     [SerializeField, NotNull] public GameObject? Prefab = default;
+
+    [Header("Exact Spawns")]
+    [SerializeField] bool SpawnExactUnits = default;
     [SerializeField, NotNull] public UnitSetup[]? Units = default;
+
+    [Header("Generator")]
+    [SerializeField, NotNull] public string? GeneratedScript = default;
+    [SerializeField] public int GeneratedCount = default;
+    [SerializeField] public Vector2 Start = default;
+    [SerializeField] public Vector2 End = default;
+    [SerializeField] public bool RandomRotation = default;
+
+    void OnValidate()
+    {
+        if (Start.x > End.x) Start.x = End.x;
+        if (Start.y > End.y) Start.y = End.y;
+    }
 
     public void Setup()
     {
@@ -38,15 +56,88 @@ public class SetupManager : Singleton<SetupManager>
             return;
         }
 
-        foreach (UnitSetup unitSetup in Units)
+        if (SpawnExactUnits)
         {
-            Entity newUnit = world.EntityManager.Instantiate(unit.Prefab);
-            world.EntityManager.SetComponentData(newUnit, LocalTransform.FromPosition(new float3(unitSetup.Spawn.x, 0.5f, unitSetup.Spawn.y)));
-            world.EntityManager.SetComponentData(newUnit, new Processor()
+            foreach (UnitSetup unitSetup in Units)
             {
-                SourceFile = new FileId(unitSetup.Script, NetcodeEndPoint.Server),
-            });
+                Entity newUnit = world.EntityManager.Instantiate(unit.Prefab);
+                world.EntityManager.SetComponentData(newUnit, LocalTransform.FromPosition(new float3(unitSetup.Spawn.x, 0.5f, unitSetup.Spawn.y)));
+                world.EntityManager.SetComponentData(newUnit, new Processor()
+                {
+                    SourceFile = new FileId(unitSetup.Script, NetcodeEndPoint.Server),
+                });
+            }
         }
+
+        StartCoroutine(SpawnRandomUnits(unit.Prefab));
+    }
+
+    IEnumerator SpawnRandomUnits(Entity prefab)
+    {
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        World world = ConnectionManager.ServerOrDefaultWorld;
+        List<float2> spawned = new(GeneratedCount);
+
+        bool IsOccupied(float2 position)
+        {
+            if (SpawnExactUnits)
+            {
+                foreach (UnitSetup unit in Units)
+                {
+                    if (math.distance(unit.Spawn, position) < 2f) return true;
+                }
+            }
+
+            foreach (float2 unit in spawned)
+            {
+                if (math.distance(unit, position) < 2f) return true;
+            }
+
+            return false;
+        }
+
+        for (int i = 0; i < GeneratedCount; i++)
+        {
+            float2 generated;
+            bool failed = true;
+            for (int j = 0; j < 10; j++)
+            {
+                yield return null;
+                generated = new(
+                    Maths.Random.Float(Start.x, End.x),
+                    Maths.Random.Float(Start.y, End.y)
+                );
+                if (IsOccupied(generated)) continue;
+                spawned.Add(generated);
+                Entity newUnit = world.EntityManager.Instantiate(prefab);
+                if (RandomRotation)
+                {
+                    world.EntityManager.SetComponentData(newUnit, LocalTransform.FromPositionRotation(
+                        new float3(generated.x, 0.5f, generated.y),
+                        quaternion.EulerXYZ(0f, Maths.Random.Float(0f, math.TAU), 0f)
+                    ));
+                }
+                else
+                {
+                    world.EntityManager.SetComponentData(newUnit, LocalTransform.FromPosition(
+                        new float3(generated.x, 0.5f, generated.y)
+                    ));
+                }
+                world.EntityManager.SetComponentData(newUnit, new Processor()
+                {
+                    SourceFile = new FileId(GeneratedScript, NetcodeEndPoint.Server),
+                });
+                failed = false;
+                break;
+            }
+            if (failed)
+            {
+                Debug.LogWarning($"Only spawned {i} but had to {GeneratedCount}");
+                break;
+            }
+        }
+
     }
 }
 
@@ -66,6 +157,17 @@ public class SetupManagerEditor : UnityEditor.Editor
             );
             t.Units[i].Spawn = new float2(p.x, p.z);
         }
+
+        Vector3 start = UnityEditor.Handles.PositionHandle(
+            new Vector3(t.Start.x, 0f, t.Start.y),
+            quaternion.identity
+        );
+        Vector3 end = UnityEditor.Handles.PositionHandle(
+            new Vector3(t.End.x, 0f, t.End.y),
+            quaternion.identity
+        );
+        t.Start = new Vector2(start.x, start.z);
+        t.End = new Vector2(end.x, end.z);
     }
 }
 #endif
