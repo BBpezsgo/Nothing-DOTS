442c2c4b17d1b022df64a6470eae53b60e86e937
BB_pezsgo <bazsi5fel2@gmail.com>
2024-12-18 13:45:20 +0100

    what???

--- Assets/RPC/CompilationAnalysticsRpc.cs
+++ Assets/RPC/CompilationAnalysticsRpc.cs
@@ -7,9 +7,9 @@ using Unity.NetCode;
 [BurstCompile]
 public struct CompilationAnalysticsRpc : IRpcCommand
 {
-    public required DiagnosticsLevel Level;
-    public required FixedString512Bytes Message;
     public FileId FileName;
     public MutableRange<SinglePosition> Position;
     public MutableRange<int> AbsolutePosition;
+    public DiagnosticsLevel Level;
+    public FixedString512Bytes Message;
 }
