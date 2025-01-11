b1aad16efc81fce95c985730e4f72452da114ede
BB_pezsgo <bazsi5fel2@gmail.com>
2025-01-11 08:58:18 +0100

    display fps

--- Assets/Mono/Client/HUDManager.cs
+++ Assets/Mono/Client/HUDManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -6,20 +7,31 @@ public class HUDManager : MonoBehaviour
 {
     [SerializeField, NotNull] UIDocument? _ui = default;
 
-    [NotNull] Label? _label = default;
+    [NotNull] Label? _labelResources = default;
+    [NotNull] Label? _labelFps = default;
+
     float _refreshAt = default;
+    float _maxDeltaTime = default;
 
     void Start()
     {
-        _label = _ui.rootVisualElement.Q<Label>("label-resources");
+        _labelResources = _ui.rootVisualElement.Q<Label>("label-resources");
+        _labelFps = _ui.rootVisualElement.Q<Label>("label-fps");
     }
 
     void Update()
     {
         float now = Time.time;
+        _maxDeltaTime = MathF.Max(_maxDeltaTime, Time.deltaTime);
         if (now < _refreshAt) return;
-        _refreshAt = now + 2f;
-        if (!PlayerManager.TryGetLocalPlayer(out Player localPlayer)) return;
-        _label.text = localPlayer.Resources.ToString();
+        _refreshAt = now + 1f;
+
+        _labelFps.text = MathF.Abs(_maxDeltaTime) < 0.01f ? "-" : MathF.Round(1f / _maxDeltaTime).ToString();
+        _maxDeltaTime = 0f;
+
+        if (PlayerManager.TryGetLocalPlayer(out Player localPlayer))
+        {
+            _labelResources.text = localPlayer.Resources.ToString();
+        }
     }
 }
