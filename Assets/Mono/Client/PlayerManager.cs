196c6fe98f1bc2f03ee5bfa75ab6832a650c22a5
BB_pezsgo <bazsi5fel2@gmail.com>
2025-05-29 12:45:33 +0200

    nothing

--- Assets/Mono/Client/PlayerManager.cs
+++ Assets/Mono/Client/PlayerManager.cs
@@ -10,68 +10,20 @@ public class PlayerManager : MonoBehaviour
         player = default;
         if (ConnectionManager.ClientOrDefaultWorld == null) return false;
 
-        return TryGetLocalPlayer(ConnectionManager.ClientOrDefaultWorld.EntityManager, out player);
-    }
-
-    public static bool TryGetLocalPlayer(EntityManager entityManager, out Player player)
-    {
         player = default;
 
-        using EntityQuery playersQ = entityManager.CreateEntityQuery(typeof(Player));
-        using EntityQuery connectionsQ = entityManager.CreateEntityQuery(typeof(NetworkId));
-        if (!connectionsQ.TryGetSingleton(out NetworkId networkId)) return false;
-
-        NativeArray<Player> players = playersQ.ToComponentDataArray<Player>(Allocator.Temp);
-        for (int i = 0; i < players.Length; i++)
-        {
-            if (players[i].ConnectionId != networkId.Value) continue;
-            player = players[i];
-            players.Dispose();
-            return true;
-        }
-
-        players.Dispose();
-        return false;
-    }
-
-    public static bool TryGetLocalPlayer(EntityManager entityManager, out Entity player)
-    {
-        player = default;
-
-        using EntityQuery playersQ = entityManager.CreateEntityQuery(typeof(Player));
-        using EntityQuery connectionsQ = entityManager.CreateEntityQuery(typeof(NetworkId));
-        if (!connectionsQ.TryGetSingleton(out NetworkId networkId)) return false;
-
-        NativeArray<Entity> players = playersQ.ToEntityArray(Allocator.Temp);
-        for (int i = 0; i < players.Length; i++)
-        {
-            var _player = entityManager.GetComponentData<Player>(players[i]);
-            if (_player.ConnectionId != networkId.Value) continue;
-            player = players[i];
-            players.Dispose();
-            return true;
-        }
-
-        players.Dispose();
-        return false;
-    }
-
-    public static Player GetLocalPlayer()
-    {
         using EntityQuery playersQ = ConnectionManager.ClientOrDefaultWorld.EntityManager.CreateEntityQuery(typeof(Player));
         using EntityQuery connectionsQ = ConnectionManager.ClientOrDefaultWorld.EntityManager.CreateEntityQuery(typeof(NetworkId));
-        if (!connectionsQ.TryGetSingleton<NetworkId>(out NetworkId networkId))
-        {
-            throw new System.Exception("Local network id not found");
-        }
+        if (!connectionsQ.TryGetSingleton(out NetworkId networkId)) return false;
 
         using NativeArray<Player> players = playersQ.ToComponentDataArray<Player>(Allocator.Temp);
         for (int i = 0; i < players.Length; i++)
         {
             if (players[i].ConnectionId != networkId.Value) continue;
-            return players[i];
+            player = players[i];
+            return true;
         }
 
-        throw new System.Exception($"Player with connection id {networkId} not found");
+        return false;
     }
 }
